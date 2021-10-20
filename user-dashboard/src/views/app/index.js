e57782cddb9630b25d0ed6604a1629f48d2f2e3e
Nishanth Bhat <nishanthbhat07@gmail.com>
2021-10-20 20:12:17 +0530

    Added routes to the dashboard

--- user-dashboard/src/views/app/index.js
+++ user-dashboard/src/views/app/index.js
@@ -4,14 +4,30 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import AppLayout from "../../layout/AppLayout";
 const UserOnBoarding = React.lazy(() => import("./user-onboarding"));
 
+const MyData = React.lazy(() => import("./my-data"));
+const MyPermission = React.lazy(() => import("./permissions"));
+const MyPrescriptions = React.lazy(() => import("./prescriptions"));
 const App = ({ match }) => {
   return (
     <AppLayout match={match}>
       <Suspense fallback={<div className="loading" />}>
         <Switch>
+          <Redirect exact from={`${match.url}/`} to={`${match.url}/my-data`} />
           <Route
             path={`${match.url}/user-on-boarding`}
             render={(props) => <UserOnBoarding {...props} />}
+          />
+          <Route
+            path={`${match.url}/my-data`}
+            render={(props) => <MyData {...props} />}
+          />
+          <Route
+            path={`${match.url}/my-permissions`}
+            render={(props) => <MyPermission {...props} />}
+          />
+          <Route
+            path={`${match.url}/my-prescriptions`}
+            render={(props) => <MyPrescriptions {...props} />}
           />
         </Switch>
       </Suspense>

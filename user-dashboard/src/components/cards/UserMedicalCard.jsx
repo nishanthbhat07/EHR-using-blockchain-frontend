35c7f8978afc5da264bbb12e9b8ffc71bdbcd3c1
Nishanth Bhat <nishanthbhat07@gmail.com>
2021-10-28 12:41:50 +0530

    Added UserMedicalCard. Populated the state for both the cards

---
+++ user-dashboard/src/components/cards/UserMedicalCard.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect } from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Input,
+  Label,
+  FormGroup,
+  Button,
+  Form,
+} from "reactstrap";
+import UseronboardingImage from "../../views/app/user-onboarding/user-onboardingImage";
+import { Separator } from "../common/Separator";
+import { Transform, Fade } from "react-animation-components";
+const UserMedicalCard = ({ state, handleChange, calculateBMI, onFinish }) => {
+  useEffect(() => {
+    calculateBMI();
+  }, [state.height, state.weight]);
+  return (
+    <Card className="d-flex flex-row mb-4 p-2">
+      <div className="user_onboaring__leftside">
+        <UseronboardingImage />
+      </div>
+
+      <div className="user_onboaring__rightside">
+        <Fade in>
+          <Card className="inner-card">
+            <div className="justify-content-center pl-3 ml-3">
+              <CardHeader>User Medical Details</CardHeader>
+              <Separator className="separator-dark mb-5" />
+              <Form>
+                <FormGroup className="form-group ">
+                  <Label>Weight in KGs</Label>
+                  <Input
+                    type="number"
+                    name="weight"
+                    value={state.weight}
+                    onChange={(e) => handleChange(e)}
+                    className="form-control"
+                    required
+                  />
+                </FormGroup>
+                <FormGroup className="form-group ">
+                  <Label>Height in CMs</Label>
+                  <Input
+                    type="number"
+                    value={state.height}
+                    onChange={(e) => handleChange(e)}
+                    name="height"
+                    className="form-control"
+                    required
+                  />
+                </FormGroup>
+                <FormGroup className="form-group ">
+                  <Label>BMI</Label>
+                  <Input
+                    value={state.bmi}
+                    disabled={true}
+                    type="number"
+                    name="bmi"
+                    className="form-control"
+                    required
+                  />
+                </FormGroup>
+                <FormGroup className="form-group ">
+                  <Label>Allergies</Label>
+                  <Input
+                    value={state.allergy}
+                    onChange={(e) => handleChange(e)}
+                    type="textarea"
+                    name="allergy"
+                  />
+                </FormGroup>
+                <FormGroup className="form-group ">
+                  <Label>Any previous medical history</Label>
+                  <Input
+                    value={state.medical_history}
+                    onChange={(e) => handleChange(e)}
+                    type="textarea"
+                    name="medical_history"
+                  />
+                </FormGroup>
+                <Button
+                  onClick={onFinish}
+                  className={`btn btn-multiple-state signup-button-container`}
+                >
+                  <span className="spinner d-block">
+                    <span className="bounce1" />
+                    <span className="bounce2" />
+                    <span className="bounce3" />
+                  </span>
+                  <span className="label">Submit</span>
+                </Button>
+              </Form>
+            </div>
+          </Card>
+        </Fade>
+      </div>
+    </Card>
+  );
+};
+export default UserMedicalCard;
--- user-dashboard/src/components/cards/UserOnBoardingCard.jsx
+++ user-dashboard/src/components/cards/UserOnBoardingCard.jsx
@@ -9,25 +9,37 @@ import {
   Button,
   Form,
 } from "reactstrap";
+import UseronboardingImage from "../../views/app/user-onboarding/user-onboardingImage";
 import { Separator } from "../common/Separator";
-const UserOnBoardingCard = () => {
+
+const UserOnBoardingCard = ({ history, state, handleChange, changePage }) => {
   return (
     <Card className="d-flex flex-row mb-4 p-2">
-      <div className="d-flex flex-grow-1 min-width-zero">
-        <CardBody className=" pl-0 align-self-center  flex-sm-row pl-3">
-          <CardHeader style={{ fontSize: 24, marginTop: 15 }}>
-            User On-Boarding Form
-          </CardHeader>
-          <Separator className="mb-5" />
+      <div className="user_onboaring__leftside">
+        <UseronboardingImage />
+      </div>
+
+      <div className="user_onboaring__rightside">
+        <Card className="inner-card">
           <div className="justify-content-center pl-3 ml-3">
+            <CardHeader>User Details</CardHeader>
+            <Separator className="separator-dark mb-5" />
             <Form>
               <FormGroup className="form-group ">
                 <Label>Full Name</Label>
-                <Input name="name" className="form-control" required />
+                <Input
+                  name="name"
+                  value={state.name}
+                  onChange={(e) => handleChange(e)}
+                  className="form-control"
+                  required
+                />
               </FormGroup>
               <FormGroup className="form-group ">
                 <Label>Address</Label>
                 <Input
+                  value={state.address}
+                  onChange={(e) => handleChange(e)}
                   type="textarea"
                   name="address"
                   className="form-control"
@@ -37,6 +49,8 @@ const UserOnBoardingCard = () => {
               <FormGroup className="form-group ">
                 <Label>Contact</Label>
                 <Input
+                  value={state.contact}
+                  onChange={(e) => handleChange(e)}
                   type="text"
                   name="contact"
                   className="form-control"
@@ -46,6 +60,8 @@ const UserOnBoardingCard = () => {
               <FormGroup className="form-group ">
                 <Label>Age</Label>
                 <Input
+                  value={state.age}
+                  onChange={(e) => handleChange(e)}
                   type="text"
                   name="age"
                   className="form-control"
@@ -54,28 +70,27 @@ const UserOnBoardingCard = () => {
               </FormGroup>
               <FormGroup className="form-group ">
                 <Label>Blood Group</Label>
-                <Input type="select" name="blood_group" className="" required>
-                  <option>A +ve</option>
-                  <option>A -ve</option>
-                  <option>O +ve</option>
-                  <option>O -ve</option>
-                  <option>B +ve</option>
-                  <option>B -ve</option>
-                  <option>AB +ve</option>
-                  <option>AB -ve</option>
-                </Input>
-              </FormGroup>
-              <FormGroup className="form-group ">
-                <Label>Medical Information</Label>
                 <Input
-                  type="textarea"
-                  name="medical_info"
-                  className="form-control"
+                  value={state.blood_group}
+                  onChange={(e) => handleChange(e)}
+                  type="select"
+                  name="blood_group"
+                  className=""
                   required
-                />
+                >
+                  <option value="A +ve">A +ve</option>
+                  <option value="A -ve">A -ve</option>
+                  <option value="O +ve">O +ve</option>
+                  <option value="O -ve">O -ve</option>
+                  <option value="B +ve">B +ve</option>
+                  <option value="B -ve">B -ve</option>
+                  <option value="AB +ve">AB +ve</option>
+                  <option value="AB -ve">AB -ve</option>
+                </Input>
               </FormGroup>
               <Button
                 className={`btn btn-multiple-state signup-button-container`}
+                onClick={changePage}
               >
                 <span className="spinner d-block">
                   <span className="bounce1" />
@@ -86,7 +101,7 @@ const UserOnBoardingCard = () => {
               </Button>
             </Form>
           </div>
-        </CardBody>
+        </Card>
       </div>
     </Card>
   );
